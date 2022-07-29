577914d6e91435cbe1eebfc5aab090cca8b66a73
Razvan Chiriac <chiriac_razvan@ymail.com>
2022-07-29 18:31:31 +0300

    fetched individual pokemon from the api

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [pokemonsList, setPokemonsList] = useState([]);
 
-export default Home
+  useEffect(() => {
+    const getPokemonsData = async () => {
+      const pokemonApiUrl = "https://pokeapi.co/api/v2/pokemon?limit=150";
+      const response = await axios.get(pokemonApiUrl).catch((error) => {
+        if (error) {
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        }
+      });
+      const pokemonsResult = response.data.results;
+
+      const individualPokemons = await Promise.all(
+        pokemonsResult.map(async (pokemon, index) => {
+          const individualPokemon = await axios
+            .get(pokemon.url)
+            .catch((error) => {
+              if (error) {
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+              }
+            });
+          return { ...individualPokemon.data };
+        })
+      );
+
+      setPokemonsList(individualPokemons);
+    };
+
+    getPokemonsData();
+    console.log(pokemonsList);
+  }, []);
+  console.log(pokemonsList);
+
+  return (
+    <>
+      <div>{pokemonsList.map((pokemon) => pokemon.name + " ")}</div>
+    </>
+  );
+};
+
+export default Home;
