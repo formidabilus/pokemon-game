bfa88d484aeb9408e2adff1a8122fb2d3f52ddc0
Laurentiu CM <laurentiu.costachescu22@gmail.com>
2022-07-30 12:07:25 +0300

    Update

--- src/UI/OverLay.js
+++ src/UI/OverLay.js
@@ -6,7 +6,6 @@ const OverLay = ({ onStartGame }) => {
   return (
     <div className={classes["overlay_container"]}>
       <div className={classes["over_lay"]}>
-        <h1>Pokemon</h1>
         <h2 className={classes["overlay_title"]}>Press to start Game!</h2>
         <Button className={classes["overlay_btn"]} onClick={onStartGame}>
           Start
