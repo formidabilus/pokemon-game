333c67f685c75d77da61e24178ef289278bb4fb9
Laurentiu CM <laurentiu.costachescu22@gmail.com>
2022-08-01 12:38:54 +0300

    Add OverLay

--- src/UI/OverLay.js
+++ src/UI/OverLay.js
@@ -6,6 +6,7 @@ const OverLay = ({ onStartGame }) => {
   return (
     <div className={classes["overlay_container"]}>
       <div className={classes["over_lay"]}>
+        <h1 className={classes["overlay_title"]}>Mortal Pokemon Combat</h1>
         <h2 className={classes["overlay_title"]}>Press to start Game!</h2>
         <Button className={classes["overlay_btn"]} onClick={onStartGame}>
           Start
