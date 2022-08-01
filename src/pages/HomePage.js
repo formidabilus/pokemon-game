a70b47791e9bab77fbdeb01d827541d4918be6b4
Razvan Chiriac <chiriac_razvan@ymail.com>
2022-08-01 20:44:24 +0300

    staged HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,12 +5,7 @@ import classes from "./HomePage.module.css";
 const HomePage = () => {
   return (
     <div className={classes["home_page"]}>
-<<<<<<< HEAD
       <OverLay />
-=======
-      {/* {start && <OverLay onStartGame={handleStart} />} */}
-      <Home />
->>>>>>> fixing_error
     </div>
   );
 };
