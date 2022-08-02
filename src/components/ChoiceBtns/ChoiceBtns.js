5907308eb1d6259a11be6589fa410524d0d09d40
Laurentiu CM <laurentiu.costachescu22@gmail.com>
2022-08-02 14:41:37 +0300

    Update

--- src/components/ChoiceBtns/ChoiceBtns.js
+++ src/components/ChoiceBtns/ChoiceBtns.js
@@ -3,9 +3,13 @@ import Button from "../../UI/Button";
 import axios from "axios";
 
 const ChoiceBtns = () => {
-  const [pokemonTypes, setPokemonTypes] = useState();
+  const [pokemonTypes, setPokemonTypes] = useState([]);
+
   const getPokemonsTypes = async () => {
     const pokemonApiTypes = "https://pokeapi.co/api/v2/type/";
+    const response = await axios.getPokemonsTypes(pokemonApiTypes);
+    const pokemonResult = response.data.results;
+    console.log(pokemonResult);
   };
   return (
     <div>
