691395edd4838504afa7c032f868484892c19afc
Laurentiu CM <laurentiu.costachescu22@gmail.com>
2022-08-02 17:28:41 +0300

    Adding fetch types data

--- src/components/ChoiceBtns/ChoiceBtns.js
+++ src/components/ChoiceBtns/ChoiceBtns.js
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Button from "../../UI/Button";
 import classes from "./ChoiceBtns.module.css";
 
 const ChoiceBtns = () => {
+  const [pokemonType, setPokemonType] = useState([]);
+
+  const getPokemonType = () => {
+    const fetchPokemonData = async () => {
+      const data = await fetch("https://pokeapi.co/api/v2/type/");
+      const response = await data.json();
+      const result = response.results;
+      const sandra = await Promise.all(
+        result.map(async (types) => {
+          const typeData = await fetch(types.url);
+          const typeRes = await typeData.json();
+          return typeRes;
+        })
+      );
+      console.log(sandra);
+    };
+    setPokemonType(fetchPokemonData);
+  };
+
+  useEffect(() => {
+    getPokemonType();
+  }, []);
+
   return (
     <div className={classes["choice_button-container"]}>
       <p className={classes["choice_text"]}>Please select choice:</p>
