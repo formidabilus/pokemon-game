19685db3eebdee86125037fd5ef83bdb1db5560c
Razvan Chiriac <chiriac_razvan@ymail.com>
2022-08-02 22:29:29 +0300

    added an obj with all damage relations based on the type name

--- src/components/ChoiceBtns/ChoiceBtns.js
+++ src/components/ChoiceBtns/ChoiceBtns.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Button from "../../UI/Button";
 import classes from "./ChoiceBtns.module.css";
 
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Card from "../Card/Card";
 import ChoiceBtns from "../ChoiceBtns/ChoiceBtns";
 import {
@@ -30,6 +31,17 @@ const Home = ({ pokemonsList }) => {
 
   const leftPokemonImage = pokemonImage[0];
   const rightPokemonImage = pokemonImage[1];
+
+  const obj = pokemonTypes.map((type) => [
+    type.name,
+    type.damage_relations.double_damage_from,
+    type.damage_relations.double_damage_to,
+    type.damage_relations.half_damage_from,
+    type.damage_relations.half_damage_to,
+    type.damage_relations.no_damage_from,
+    type.damage_relations.no_damage_to,
+  ]);
+  console.log(obj);
 
   return (
     <div className={classes["home_container"]}>
