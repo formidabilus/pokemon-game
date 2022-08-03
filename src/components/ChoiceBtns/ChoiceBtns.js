ebaab122f16bf0ae9ffa52884ec9ba69d01faccd
Laurentiu CM <laurentiu.costachescu22@gmail.com>
2022-08-03 10:03:49 +0300

    Updates

--- src/components/ChoiceBtns/ChoiceBtns.js
+++ src/components/ChoiceBtns/ChoiceBtns.js
@@ -5,7 +5,7 @@ import classes from "./ChoiceBtns.module.css";
 const ChoiceBtns = () => {
   return (
     <div className={classes["choice_button-container"]}>
-      <p className={classes["choice_text"]}>Please select choice:</p>
+      <p className={classes["choice_text"]}>Please select your choice:</p>
       <Button className={classes["choice_buttons"]}>Win</Button>
       <Button className={classes["choice_buttons"]}>Draw</Button>
       <Button className={classes["choice_buttons"]}>Lose</Button>
